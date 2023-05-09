d253e2c58d1a231e1be9f66e034752c788591ea1
ngnma <n.mashayekhi456@gmail.com>
2023-05-09 20:16:09 +0330

    connected to mongoDB

---
+++ app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://admin:admin@cluster0.iviisnj.mongodb.net/Test?retryWrites=true&w=majority"
+  };
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require('mongoose')
+const app = express();
+const db = require("./app/models");
+
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
+// app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Golestan application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+db.mongoose.connect(db.url)
+.then(()=>{
+    console.log('connected to mongoDB')
+    app.listen(PORT , ()=>{
+        console.log(`application is listening on localhost:${PORT}`)
+    });
+}).catch((error)=>{
+    console.log(error)
+})
